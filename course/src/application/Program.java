dc945a329b2fdf27a14818c3a182ae97d9c4fc9f
Bruno Gonçalves <bruuno_goncalves@outlook.com.br>
2024-11-16 14:45:41 -0300

    add: progress

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -1,7 +1,27 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter account number: ");
+        int numberAccount = sc.nextInt();
+        System.out.print("Enter account holder: ");
+        String accountHolder = sc.nextLine();
+        System.out.print("Is there na initial deposit (y/n)?: ");
+        String decision = sc.nextLine();
+
+        if (decision.equals("y")) {
+            System.out.print("Enter initial deposit value: ");
+            double depositAmount = sc.nextDouble();
+        }
+        
+
+
+        sc.close();
     }
 }
--- course/src/entities/BankAccount.java
+++ course/src/entities/BankAccount.java
@@ -1,35 +1,15 @@
 package entities;
 
 public class BankAccount {
-    public int accountNumber;
+    public int numberAccount;
     public String accountHolder;
     public double balance;
 
-    public BankAccount(int accountNumber, String accountHolder) {
-
-    };
-
-    public BankAccount(int accountNumber, String accountHolder, double initialDeposit) {
-
-    }
-
-    public int getAccountNumber() {
-        return accountNumber;
-    }
-
-    public String getAccountHolder() {
-        return accountHolder;
-    }
-
-    public void setAccountHolder(String accountHolder) {
+    public BankAccount(int numberAccount, String accountHolder, double balance) {
+        this.numberAccount = numberAccount;
         this.accountHolder = accountHolder;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
         this.balance = balance;
     }
+
+
 }
