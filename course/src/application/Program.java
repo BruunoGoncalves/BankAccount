1c87bc9b92873451453e95c56684049f9659da7e
Bruno Gonçalves <bruuno_goncalves@outlook.com.br>
2024-11-16 19:37:49 -0300

    add: all project

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import entities.BankAccount;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,20 +9,43 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        BankAccount bankAccount;
 
         System.out.print("Enter account number: ");
         int numberAccount = sc.nextInt();
         System.out.print("Enter account holder: ");
+        sc.nextLine();
         String accountHolder = sc.nextLine();
         System.out.print("Is there na initial deposit (y/n)?: ");
-        String decision = sc.nextLine();
+        char decision = sc.next().charAt(0);
 
-        if (decision.equals("y")) {
+        if (decision == 'y') {
             System.out.print("Enter initial deposit value: ");
-            double depositAmount = sc.nextDouble();
+            double initialDeposit = sc.nextDouble();
+            bankAccount = new BankAccount(numberAccount, accountHolder, initialDeposit);
+        } else {
+            bankAccount = new BankAccount(numberAccount, accountHolder);
         }
-        
 
+        System.out.println();
+        System.out.println("Account data: ");
+        System.out.println(bankAccount);
+
+        System.out.println();
+        System.out.print("Enter a deposit value: ");
+        double depositAmount = sc.nextDouble();
+        bankAccount.deposit(depositAmount);
+        System.out.println();
+        System.out.println("Updated account data: ");
+        System.out.println(bankAccount);
+
+        System.out.println();
+        System.out.print("Enter a withdraw value: ");
+        double withdrawValue = sc.nextDouble();
+        bankAccount.withdraw(withdrawValue);
+        System.out.println();
+        System.out.println("Updated account data: ");
+        System.out.println(bankAccount);
 
         sc.close();
     }
--- course/src/entities/BankAccount.java
+++ course/src/entities/BankAccount.java
@@ -1,15 +1,52 @@
 package entities;
 
 public class BankAccount {
-    public int numberAccount;
-    public String accountHolder;
-    public double balance;
+    private int numberAccount;
+    private String accountHolder;
+    private double balance;
 
-    public BankAccount(int numberAccount, String accountHolder, double balance) {
+    public BankAccount(int numberAccount, String accountHolder) {
         this.numberAccount = numberAccount;
         this.accountHolder = accountHolder;
-        this.balance = balance;
     }
 
+    public BankAccount(int numberAccount, String accountHolder, double initialDeposit) {
+        this.numberAccount = numberAccount;
+        this.accountHolder = accountHolder;
+        deposit(initialDeposit);
+    }
+
+    public int getNumberAccount() {
+        return numberAccount;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount + 5.00;
+    }
+
+    public String toString() {
+        return "Account: "
+                + numberAccount
+                + ", Holder: "
+                + accountHolder
+                + ", Balance: $ "
+                + String.format("%.2f", balance);
+    }
 
 }
